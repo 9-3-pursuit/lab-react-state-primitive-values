ec23974f14bd9b53f3ba4136626d3ebea830d49c
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-02 15:17:02 -0500

    add state

---
+++ src/Components/Clicker.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const Clicker = () => {
+    const [count,setCount] = useState
+
+
+    return (
+        <div>
+            <h1> Clicker Game </h1>
+            <p> </p>
+        </div>
+    )
+}
+
+export default Clicker
