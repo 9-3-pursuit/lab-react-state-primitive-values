bd21249430cf68aa04ee407359fc5ce91a1353a3
Amber King <amber.king9292@gmail.com>
2023-02-03 15:58:54 -0500

    minor updates

--- src/App.js
+++ src/App.js
@@ -19,12 +19,17 @@ function App() {
         setCount(count - 10)
         setAdded(added + 1)
       }
+      // if (count >= 100) { //increment of 100 or more & you win
+      //   setCount(count + added)
+
+
+      // }
     }
   }
 
   function winPage(count) {
 
-    if (count >= 100) {
+    if (count >= 100) { //increment of 100 or more & you win
       setCount(count + added)
 
 
@@ -36,10 +41,14 @@ function App() {
     setAdded(1)
   }
   return (
+
     <main>
+      
       <h2>Current Score: {count}</h2>
       <button onClick={increaseAmt}>+{added}</button>
       <button onClick={pointsPaid}>Pay 10 points to change from +{added} to +{added + 1}</button>
+      {/* <h3 style={font ? { color: "black" } : { color: "purple" }}> */}
+      <button style={count ? {display:'none'} : {display:'block'}}></button>
       <h2>You Win!</h2>
       <button onClick={restartGame}>Play again?</button>
     </main>
