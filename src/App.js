89465a112dd79ba4926ff74c0eddf940574cab42
Tafari Excell <excell.tafari@gmail.com>
2023-02-03 12:36:03 -0500

    Layout complete with tests passing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
-
+import {useState} from "react"
 function App () {
+  const [count, setCounter] = useState(0)
+  function setCountAndIncrementer () {
+    let count = 0;
+
+  }
+
     return (
       <main>
         <p>React State Lab</p>
+        <h2>Current Score</h2>
+        {/* <button onClick={setCountAndIncrementer}>Counter</button> */}
+        <button>+1</button>
+        <button>Pay 10 points to change from +1 to +2</button>
       </main>
     );
 }
