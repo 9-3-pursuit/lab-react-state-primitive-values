0bd43f187f6aaeeaea567b802ca312cb05ff4b4b
HaroldF415 <harold.f415@gmail.com>
2023-02-02 22:33:52 -0500

    mise en place

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./App.css";
 
+// importing da style
+import "./index.css";
+
+// importing state
+import { useState } from "react";
+
+// remember state is immutable
+// Mise en place
+const [score, setScore] = useState(0);
+const [point, setPoint] = useState(1);
+
 const App = () => {
   return (
     <main>
+      {/* This `div` will hold the main components of the game */}
       <div className="main-game-container"></div>
+
+      {/* This `div` will hold the components after the game is won */}
       <div className="game-won-container"></div>
     </main>
   );
