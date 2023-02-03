9398f319c36595bc38fc7b4480f45706d5785990
HaroldF415 <harold.f415@gmail.com>
2023-02-02 22:41:22 -0500

    fulfullied readme.md instructions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
 import "./App.css";
 
-// importing da style
-import "./index.css";
 
-// importing state
+import "./index.css";
 import { useState } from "react";
 
-// remember state is immutable
-// Mise en place
 const [score, setScore] = useState(0);
 const [point, setPoint] = useState(1);
 
 const App = () => {
   return (
     <main>
-      {/* This `div` will hold the main components of the game */}
-      <div className="main-game-container"></div>
+      <div className="main-game-container">
+        <h1>Current Score: {score}</h1>
+        <button onClick={}>+{point}</button>
+        <button onClick={}>
+          Pay 10 points to change from +{point} to +{point + 1}
+        </button>
+      </div>
 
-      {/* This `div` will hold the components after the game is won */}
-      <div className="game-won-container"></div>
+      <div className="game-won-container">
+        <h2>You Win!</h2>
+        <button type="submit" onClick={}>
+          Play again?
+             </button>
+      </div>
     </main>
   );
 };
