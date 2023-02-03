f2ffb0d65ce20a998e271faa6b0e69f66d8a4f95
HaroldF415 <harold.f415@gmail.com>
2023-02-02 22:42:59 -0500

    setting up function names and their es6 counterparts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
 import "./App.css";
 
-
 import "./index.css";
 import { useState } from "react";
 
 const [score, setScore] = useState(0);
 const [point, setPoint] = useState(1);
 
+const updateScore = () => {};
+const updatePoints = () => {};
+const reset = () => {};
+
 const App = () => {
   return (
     <main>
       <div className="main-game-container">
         <h1>Current Score: {score}</h1>
-        <button onClick={}>+{point}</button>
-        <button onClick={}>
+        <button onClick={updateScore}>+{point}</button>
+        <button onClick={updatePoints}>
           Pay 10 points to change from +{point} to +{point + 1}
         </button>
       </div>
 
       <div className="game-won-container">
         <h2>You Win!</h2>
-        <button type="submit" onClick={}>
+        <button type="submit" onClick={reset}>
           Play again?
-             </button>
+        </button>
       </div>
     </main>
   );
