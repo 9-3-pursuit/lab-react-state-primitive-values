0e73484a3d3f15f114af6bf2cb24df4166a46299
Ryan Lundy <rlundy1792@gmail.com>
2023-02-03 12:36:16 -0500

    Passing all tests for the layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react"
 
 function App () {
     return (
       <main>
         <p>React State Lab</p>
+        <h2>Current Score: </h2>
+        <button>+1</button>
+        <button>Pay 10 points to change from +1 to +2"</button>
       </main>
     );
 }
