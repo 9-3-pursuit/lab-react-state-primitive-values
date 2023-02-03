37a7a9ca5310ff44d04edd59b2085a14534bdcb1
Amber King <amber.king9292@gmail.com>
2023-02-03 16:33:38 -0500

    added on hidden and changeable page settings

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import { useState } from "react"
 
 function App() {
-  // let newScore = 0
   const [count, setCount] = useState(0) // score button info
   const [added, setAdded] = useState(1) //increment button info
   function increaseAmt() {
@@ -19,22 +18,22 @@ function App() {
         setCount(count - 10)
         setAdded(added + 1)
       }
-      // if (count >= 100) { //increment of 100 or more & you win
-      //   setCount(count + added)
+      if (count >= 100) { //increment of 100 or more & you win
+        setCount(count + added)
 
 
-      // }
+      }
     }
   }
 
-  function winPage(count) {
+  // function winPage(count) { 
 
-    if (count >= 100) { //increment of 100 or more & you win
-      setCount(count + added)
+  //   if (count >= 100) { //increment of 100 or more & you win
+  //     setCount(count + added)
 
 
-    }
-  }
+  //   }
+  // }
 
   function restartGame() {
     setCount(0)
@@ -43,14 +42,24 @@ function App() {
   return (
 
     <main>
-      
-      <h2>Current Score: {count}</h2>
-      <button onClick={increaseAmt}>+{added}</button>
-      <button onClick={pointsPaid}>Pay 10 points to change from +{added} to +{added + 1}</button>
+      {count < 100 ? ( //allows for hide of currentscore text & increment button 
+        <div>
+          <h2>Current Score: {count}</h2>
+          <button onClick={() => increaseAmt(count)}>+{added}</button>
+          <button onClick={() => pointsPaid(count)}>Pay 10 points to change from +{added} to +{added + 1}</button>
+        </div>
+
+      ) : (
+        <div>
+          <h2>You Win!</h2>
+          <button onClick={restartGame}>Play again?</button>
+        </div>
+
+      )}
+
       {/* <h3 style={font ? { color: "black" } : { color: "purple" }}> */}
       {/* <button style={count ? {display:'none'} : {display:'block'}}></button> */}
-      <h2>You Win!</h2>
-      <button onClick={restartGame}>Play again?</button>
+
     </main>
   );
 }
