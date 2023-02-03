061acace51af8074d464239b74992d525686346e
Marangely Rosas <Ladykaliber22@gmail.com>
2023-02-03 15:15:29 -0500

    Completed Lab-React-State-Primitive-Values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React from "react";
 import "./App.css";
+import {useState} from "react";
+
 
 function App () {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [x, setX] = useState(1);
+
+function incrementByX() {
+    //console.log("This will increase the score by 1");
+    setCurrentScore(currentScore + x);
+    
+  }
+
+  function incrementChange() {
+    if (currentScore < 10) {
+     alert("You can't afford that!"); 
+    } else {
+      setX(x + 1)
+      setCurrentScore (currentScore - 10);
+    }
+  }
+
+  function playAgain() {
+    setX(1);
+    setCurrentScore(0);
+  }
+  
     return (
-      <main>
-        <p>React State Lab</p>
+      <main> 
+        <div style={(currentScore < 100) ? {display: 'block'} : {display: 'none'}}>
+        <h2>Current Score: {currentScore}</h2>
+        <button onClick={incrementByX}>+{x}</button>
+        <button onClick={incrementChange}>Pay 10 points to change from +{x} to +{x +1}</button>
+        </div>
+        <div style={(currentScore < 100) ? {display: 'none'} : {display: 'block'}}>
+        <h2>Current Score: {currentScore}</h2>
+        <h2> You Win! </h2>
+        <button onClick={playAgain}> Play again?</button>
+        </div>
       </main>
     );
 }
