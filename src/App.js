453a617c93eb5b7178194dc68a0a0287e0bb8df5
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-04 17:52:02 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
+  const [count, setCount] = useState(0)
+  console.log(setCount, count)
+
+  const handleClick = () => {
+    setCount(count + 1)
+    console.log(setCount, count)
+  }
     return (
       <main>
         <p>React State Lab</p>
+        <h1> Current Score: {count}</h1>
+        <button onClick={() => handleClick()}> +1 </button>
       </main>
     );
 }
