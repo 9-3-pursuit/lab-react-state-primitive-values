6131d7ae856d50c125dbb2a328f549c245df1856
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-04 18:33:56 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ function App () {
     setScore(score + 1)
     console.log(setScore, score)
   }
+
+
+  const resetScore = () => {
+    setScore(0)
+    
+  }
     return (
       <main>
         <p>React State Lab</p>
         <h1> Current Score: {score}</h1>
         <button onClick={() => handleClick()}> +1 </button>
         <button onClick={() => handleClick()}> Pay 10 points to change from +{score} to +{score + 1} </button>
+        <button onClick={() => resetScore()}> Play again </button>
       </main>
     );
 }
