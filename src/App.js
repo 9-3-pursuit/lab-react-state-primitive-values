67812171dfabde1de05d1ce4676ac9fa49ad3f97
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-02-03 22:34:46 -0500

    added some data to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from "react";
 import "./App.css";
+import {render} from "@testing-library/react";
 
-function App () {
-    return (
+
+class App extends React.Component {
+
+
+// function App () {
+  render() {
+    return  (
+
+    
+
       <main>
         <p>React State Lab</p>
+        <p></p>
       </main>
     );
+    
+}
+constructor (){
+
+  super();
+  this.state = {
+    score: 0,
+    addButton: 1,
+  };
+}
+upgrade () {
+
+  if(this.state.score >= 10){
+     this.setState({addButton: this.state.addButton + 1});
+     this.setState({score: this.state.score - 10});
+
+  }else{
+    alert("You can't afford that!");
+
+  }
+}
+increment() {
+  this.setState({score: this.state.score + this.state.addButton});
+}
+restart () {
+  
 }
 
 export default App;
