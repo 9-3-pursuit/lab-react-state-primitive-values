768265e910458a87776628b0a0c75d2c62fdab1a
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-02-05 05:59:46 -0500

    added additional data and passed some tests

--- src/App.js
+++ src/App.js
@@ -42,7 +42,31 @@ increment() {
   this.setState({score: this.state.score + this.state.addButton});
 }
 restart () {
-  
-}
+ 
+  console.log("restarting");
+  this.setState({score: 0});
+  this.setState({addButton: 1})
 
+}
+render(){
+return this.state.score < 100 ? (
+  <main>
+
+    <h1> Current Score: {this.state.score}</h1>
+    <button onClick={() => this.increment()}>+{this.state.addButton}</button>
+    <button onClick={() => this.upgrade()}>
+      Pay 10 points to change from + {this.state.addButton} to + {this.state.addButton + 1}
+    </button>
+  </main>
+  ):(
+     <main>
+
+      <h1>{this.state.score}</h1>
+      <h2>You Win!</h2>
+      <button onClick={() => this.restart()}>Play again?</button>
+
+     </main>
+  );
+}
+}
 export default App;
