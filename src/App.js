b0f31b63cf8c90d51e3cd5ad42bda1db7a4d798b
mrkwab3 <mrkwab3@gmail.com>
2023-02-05 21:56:09 -0500

    Clicking '+1' increments current score by 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+  const [points, setPoints] = useState(0)
+  const [spend, setSpend] = useState(10)
+
+  function earnPoints() {
+    setPoints(points + 1)
+  }
+
+  function spendPoints() {
+    
+  }
+  
+  return (
+    <main>
+      <h1>Current Score: {points}</h1>
+      <button onClick={earnPoints}>+1</button>
+      <br />
+      <button>Pay 10 points to change from +1 to +2</button>
+    </main>
+  );
 }
 
 export default App;
