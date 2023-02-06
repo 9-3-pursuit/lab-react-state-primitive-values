1dc8a8fd6cdbd2aa53c9180ef1ad12efbd247789
Wilton <wiltoncamilol@gmail.com>
2023-02-06 10:40:04 -0400

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from "react";
 import "./App.css";
-
+import {useState} from "react";
 function App () {
+  
+  const [sum, setSum] = useState(0)
+  const [x, setX] = useState(1)
+
+  function addX (){
+    if (sum >= 100){
+      <h2>You Win!</h2>
+    }
+   setSum(sum +x)
+  }
+  
+  function payment(){
+    if(sum >= 10){
+      setSum(sum - 10) 
+      setX(x+1)
+    } else {
+      alert("You can't afford that!")
+    }
+  }
+
     return (
       <main>
-        <p>React State Lab</p>
+        <h1>Current score: {sum} </h1>
+        <button onClick = {addX}>+{x}</button>
+        <button onClick = {payment}>Pay 10 points to change from +{x} to +{x+1}</button>
       </main>
     );
 }
