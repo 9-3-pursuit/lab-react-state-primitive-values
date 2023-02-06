9c554da109d80869c4a5fd9a3daa06e2a1dc229a
mrkwab3 <mrkwab3@gmail.com>
2023-02-05 22:12:51 -0500

    When user reaches 100+ score, they win and receive the option to play again

--- src/App.js
+++ src/App.js
@@ -13,15 +13,23 @@ function App () {
   function spendPoints() {
     if (points < spend) {
       alert('You can\'t afford that!')
+    } else {
+      setPoints(points - 10)
     }
 
+    
 
     // setSpend(spend + 10)
     // for (let i = 10; i <= spend; i += 10) {
 
     // }
   }
+
+  function playAgain() {
+    setPoints(0)
+  }
   
+  if (points < 100) {
   return (
     <main>
       <h1>Current Score: {points}</h1>
@@ -30,6 +38,14 @@ function App () {
       <button onClick={spendPoints}>Pay 10 points to change from +1 to +2</button>
     </main>
   );
+  } else
+  return (
+    <main>
+      <h1>Current Score: {points}</h1>
+      <h2>You win!</h2>
+      <button onClick={playAgain}>Play Again?</button>
+    </main>
+  )
 }
 
 export default App;
