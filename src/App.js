cad83ffd83ef5a4e533722145fde51e22ed36cd4
SangUn Park <Sangunpark1@gmail.com>
2023-02-05 20:32:50 -0500

    lab finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
+
 
 function App () {
-    return (
-      <main>
-        <p>React State Lab</p>
-      </main>
-    );
+  const [counter, setCounter] = useState(0);
+  const [accumulator, setAccumulator] = useState(1)
+  
+  function addAccumulation() {
+    if (counter < 10) {
+      alert("You can't afford that!");
+      return;
+    }
+    setAccumulator(accumulator + 1);
+    setCounter(counter - 10);
+  }
+  const countClick = () => setCounter(counter + accumulator)
+
+  function resetGame() {
+    setCounter(0);
+    setAccumulator(1);
+  }
+
+  let gameInPlay = (
+    <>
+      <h1>Current Score: {counter}</h1>
+      <button onClick={countClick}>+{accumulator}</button>
+      <button onClick={addAccumulation}>
+        Pay 10 points to change from +{accumulator} to +{accumulator + 1}
+      </button>
+    </>
+  );
+
+  let youWon = (
+    <div>
+      <h2>You Win! </h2>
+      <button onClick={resetGame}> Play again? </button>
+    </div>
+  );
+
+    return <main>{counter < 100 ? gameInPlay : youWon}</main>;
 }
 
 export default App;
