63046a6c6a64277b902759cd2858b6d7e02f4c59
wal3sca <walescaambroise@gmail.com>
2023-02-07 11:18:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
 import React from "react";
 import "./App.css";
 
-function App () {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentScore: 0,
+      incrementScore: 1,
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      currentScore: (this.state.currentScore += this.state.incrementScore),
+    });
+  };
+
+  // event handler
+  incrementBy1DecByCurrent = () => {
+    this.setState({
+      incrementScore: this.state.incrementScore + 1,
+    });
+    this.setState({ currentScore: this.state.currentScore - 10 });
+    console.log(this.state.currentScore);
+  };
+
+  // alert message
+  alertMessage = () => {
+    alert("You can't afford that!");
+  };
+
+  // reset game
+  resetGame = () => {
+    this.setState({
+      currentScore: 0,
+      incrementScore: 1,
+    });
+  };
+
+  render() {
     return (
       <main>
-        <p>React State Lab</p>
+        {this.state.currentScore < 100 ? (
+          <div>
+            <p>Current Score: {this.state.currentScore}</p>
+            <button onClick={this.increment}>
+              {" "}
+              +{this.state.incrementScore}
+            </button>
+            <button
+              onClick={() =>
+                this.state.currentScore >= 10
+                  ? this.incrementBy1DecByCurrent()
+                  : this.alertMessage()
+              }
+            >
+              Pay 10 points to change from +{this.state.incrementScore} to +
+              {this.state.incrementScore + 1}
+            </button>
+          </div>
+        ) : (
+          <div>
+            You Win!
+            <button onClick={() => this.resetGame()}>Play again?</button>
+          </div>
+        )}
       </main>
     );
+  }
 }
 
 export default App;
