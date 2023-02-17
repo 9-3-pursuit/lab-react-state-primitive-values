d65598d2436bc588c7ce9f7c5ff6a97fd9177bbb
Tafari Excell <excell.tafari@gmail.com>
2023-02-17 04:41:53 -0500

    first attempt

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
 import React from "react";
 import "./App.css";
-import {useState} from "react"
-function App () {
+import { useState } from "react"
+function App() {
 
- 
+
 
   const [count, setCount] = useState(0);
+
   const [incrementorButton, setIncrementorButton] = useState(1);
+ 
   // const [points, payForPoints] = useState(true)
   const [buttonHides, setButtonHides] = useState(true)
-  function keepScores () {
-  
+// function startingScore () {
+ 
+//   let currentScore= `Current Score:${score}`;
+//   return currentScore;
+
+// } 
+
+
+
+  function keepScores() {
+    
     setCount(count + incrementorButton);
-  
+
   }
 
-  function payForPoints () {
+  function payForPoints() {
     if (count < 10) {
       alert("You can't afford that!")
-    }else if (count >=10) {
-      
-       setIncrementorButton(incrementorButton + 1);
-       setCount(count - 10);
+    } else if (count >= 10) {
+
+      setIncrementorButton(incrementorButton + count);
+      setCount(count - 10);
      
     }
+    return setIncrementorButton;
 
   }
 
-  function victoryPage () {
+  function victoryPage() {
     let victoryText = "You win!"
     if (count >= 100) {
       return victoryText;
-      
+
     }
   }
 
   function buttonDisplayTask() {
-    if(useState === true) {
+    if (useState === true) {
       setButtonHides = false
     }
   }
@@ -47,20 +59,28 @@ function App () {
 
 
 
-    return (
-      <main>
-        {count < 100}
-        <h2 class="text">Current Score:<span class="score">{ count }</span></h2>
-        {/* <button onClick={setCountAndIncrementer}>Counter</button> */}
-        <button onClick= { keepScores }>+{ incrementorButton }</button>
-        <button onClick= { payForPoints }>Pay 10 points to change from +{ setIncrementorButton } to  {incrementorButton + 1 }<div> { setButtonHides }</div></button>
-        <br></br>
-        <br></br>
-        <span>You win!</span>
-        <br></br>
-        <button onClick="replay!">Play Again?</button>
-      </main>
-    );
+  return (
+    <main>
+       
+
+      {count < 100 ? (
+        <div>
+         
+          <h2 class="text">Current Score: <span class="score">{count}</span></h2>
+          <br>
+          </br>
+          <button onClick={keepScores}> +1</button>
+          <button type="submit" value="setIncrementorButton +2"onClick={payForPoints}>Pay 10 points to change from +{setIncrementorButton} to {incrementorButton +1}</button>
+          
+        </div>) :(
+        <div>
+          <span>You win!</span>
+          <button onClick="replay!">Play Again?</button>
+        </div>
+        )
+      }
+    </main>
+  );
 }
 
 export default App;
