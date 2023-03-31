ba496cf90580b3a2a99be29a75f3003a26520905
Karimah Reavis <Karimah586@gmail.com>
2023-03-31 10:19:39 -0400

    more features working

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ function App () {
   function addOne(){
     const selectIndex = num +1
     setNum(selectIndex)
-    if(num >= 10){
-      const addTen = num + 10
-      setNum(addTen)
-    }
+   
   }
+      function payTen(){
+         if(num < 10){
+          alert("You can't afford that!")
+         }
+         else if(num >= 10){
+          const subTen = (num - 10)
+          setNum(subTen)
+         }
+        }
     return (
       <main>
         <p>Current Score:{num}</p>
         <button onClick={addOne}>+1</button>
+        <br></br>
+        <br></br>
+        <button onClick={payTen}>Pay 10 points to change from +1 to +2</button>
       </main>
     );
 }
