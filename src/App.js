57d5828a72798ed6ccffb122df46608ae41554a3
Karimah Reavis <Karimah586@gmail.com>
2023-03-31 09:05:54 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App () {
+  const [num, setNum] = useState(0)
+  function addOne(){
+    const selectIndex = num +1
+    setNum(selectIndex)
+    if(num >= 10){
+      const addTen = num + 10
+      setNum(addTen)
+    }
+  }
     return (
       <main>
-        <p>React State Lab</p>
+        <p>Current Score:{num}</p>
+        <button onClick={addOne}>+1</button>
       </main>
     );
 }
