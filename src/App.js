e5026bbc0006f1752826d0afe39a93d58e195d21
Karimah Reavis <Karimah586@gmail.com>
2023-03-31 22:31:59 -0400

    more working parts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 function App () {
   const [num, setNum] = useState(0)
+  const [plusOne, setPlusOne] = useState("+1")
+  const [payBtn, setPayBtn] = useState("Pay 10 points to change from +1 to +2")
   function addOne(){
     const selectIndex = num +1
     setNum(selectIndex)
@@ -13,18 +15,26 @@ function App () {
          if(num < 10){
           alert("You can't afford that!")
          }
-         else if(num >= 10){
+         if(num >= 10){
           const subTen = (num - 10)
           setNum(subTen)
-         }
+          const changeTen = "+2"
+          setPlusOne(changeTen)
+          const changePayBn = "Pay 10 points to change from +2 to +3"
+          setPayBtn(changePayBn)
+        }
+         if(plusOne === "+2"){
+          const addTwo= (num +2)
+          setNum(addTwo)
+         } 
         }
     return (
       <main>
         <p>Current Score:{num}</p>
-        <button onClick={addOne}>+1</button>
+        <button onClick={addOne}>{plusOne}</button>
         <br></br>
         <br></br>
-        <button onClick={payTen}>Pay 10 points to change from +1 to +2</button>
+        <button onClick={payTen}>{payBtn}</button>
       </main>
     );
 }
